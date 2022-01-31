a20d11f2548f903048245fa7dd21799fc8222a48
ethamoos <amosdeane@gmail.com>
2022-01-31 18:01:41 +0000

    Added more info and description

--- GithubLookupCorrected/ContentView.swift
+++ GithubLookupCorrected/ContentView.swift
@@ -1,8 +1,8 @@
 //
-//  ContentView.swift
-//  GIthubLookupCorrected
 //
-//  Created by Amos Deane on 27/08/2021.
+//  This is an extension of an exercise is Greg Lim's book Beginning Swift UI https://www.amazon.com/Beginning-SwiftUI-Greg-Lim-ebook/dp/B08XV4NPDP
+// Thanks to those on StackOverflow for help making it work - https://stackoverflow.com/questions/68949231/issue-with-defining-a-variable-via-a-textfield-and-then-passing-it-to-an-observe
+//
 //
 
 import SwiftUI
